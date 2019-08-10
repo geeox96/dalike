b443b3bc42941a7001918565a3175715ef75442e
geeox96 <geeox96@gmail.com>
2019-08-10 09:12:54 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,57 @@
+/**
+ * Social Manager Tools: Instagram Bot API
+ * =====================
+ * Scraping instagram website with love and nodejs
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ *
+ * @license: This code and contributions have 'MIT License'
+ *           Permission is hereby granted, free of charge, to any person obtaining a copy
+ *           of this software and associated documentation files (the "Software"), to deal
+ *           in the Software without restriction, including without limitation the rights
+ *           to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *           copies of the Software, and to permit persons to whom the Software is
+ *           furnished to do so, subject to the following conditions:
+ *
+ *           The above copyright notice and this permission notice shall be included in all
+ *           copies or substantial portions of the Software.
+ *
+ *           THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *           IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *           FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *           AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *           LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *           OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *           SOFTWARE.
+ *
+ * @link:    Homepage: https://socialmanager.tools
+ *           Docs:     https://docs.socialmanager.tools
+ *           NPM:      https://www.npmjs.com/package/socialmanagertools-igbot
+ *           GitHub:   https://github.com/social-manager-tools/socialmanagertools-igbot
+ *
+ */
+
+const argv = require("yargs").argv;
+const config = (argv.config ? require(argv.config) : require("./configs/config.js"));
+
+const Bot = require("./modules/core/lib");
+let bot = new Bot(config);
+
+(async() => {
+	await bot.start();
+
+	let api = await bot.api();
+
+	let response = await api.login.flow();
+
+	if (response.status) {
+		response = await api.twofa.flow();
+	}
+
+	if (response.status) {
+		response = await api.mode.flow();
+	}
+
+	// await bot.stop();
+})();
---
+++ modules/routes/modes.js
@@ -0,0 +1,23 @@
+/**
+ * Routes
+ * =====================
+ * Possible strategy / modes
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *                Ilya Chubarov [@agoalofalife] <agoalofalife@gmail.com>
+ * @license: MIT License
+ *
+ */
+module.exports = {
+	"Likemode_classic": require("../modes/likemode_classic"),
+	"Likemode_classic_location": require("../modes/likemode_classic_location"),
+	"Likemode_realistic": require("../modes/likemode_realistic"),
+	"Likemode_realistic_location": require("../modes/likemode_realistic_location"),
+	"Likemode_superlike": require("../modes/likemode_superlike"),
+	"Likemode_superlike_location": require("../modes/likemode_superlike_location"),
+	"Commentmode_realistic": require("../modes/commentmode_realistic"),
+	"Commentmode_realistic_location": require("../modes/commentmode_realistic_location"),
+	"Fdfmode_realistic": require("../modes/fdfmode_realistic"),
+	"Fdfmode_realistic_location": require("../modes/fdfmode_realistic_location"),
+	"Likemode_competitor_users": require("../modes/likemode_competitor_users")
+};
